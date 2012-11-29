ba0f7b3d12798155418513c1a52a35a7b90cac0a
Brett Clarke <brett-john@hotmail.com>
2012-11-29 15:04:11 +0700

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realecloud::Application.config.secret_token = 'bafb587b88c8e612779854d2a83d437f8a401387ea3e4741ba91acfd2da12a9c8dfdc581acc2f6362c82d283a8ff4616b3aae5a88645f1abb434e00080e6d6dc'
